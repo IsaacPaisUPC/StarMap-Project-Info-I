47ebfe049e8943d5d30961e9dd5f4b3203acc678
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2025-12-08 00:58:59 +0100

    Add addConstellation function to starmap
    Implement addConstellation function to add a new constellation to the starmap.

--- funcions1.py
+++ funcions1.py
@@ -1,3 +1,13 @@
+"""
+Tasca1
+Dissenyeu unaf unció de nom addConstellation(starmap,constellation)que,donat un mapa
+estel·lar i un nom de constel·lació, afegeixi una nova constel·lació buida al mapa.
+>>>starmap={}
+>>>addConstellation(starmap,"GreatBear")
+>>>starmap
+{'GreatBear':{'stars':{},'adjacencies':{}}}
+
+"""
 starmap = {}
 constellation = input("Diguem una constel·lació: ")
 
