57707119cf4f350241af9c0a37764fedff436159
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2025-12-08 01:32:05 +0100

    Refactor addStars function for better validation
    Updated the addStars function to improve checks for constellation existence and star duplication.

--- funcions2.py
+++ funcions2.py
@@ -1,27 +1,22 @@
 """
 Tasca2
-Dissenyeu una funció de nom addStars(starmap,constellation,starNames) que,donat un
-mapa estel·lar, una constel·lació i una llista d’estrelles , permeti afegir les estrelles donades a una
+Dissenyeu una funció de nom addStars(starmap,constellation,starNames) que, donat un
+mapa estel·lar, una constel·lació i una llista d’estrelles, permeti afegir les estrelles donades a una
 constel·lació existent. Cada estrella ha de començar sense adjacències i amb coordenades buides.
->>>starmap={'GreatBear':{'stars':{},'adjacencies':{}}}
->>>addStars(starmap,"GreatBear", ["Alkaid","Dubhe"])
->>>starmap
-{'GreatBear':{'stars':{'Alkaid': [],'Dubhe':[]},
-...'adjacencies':{'Alkaid':[],'Dubhe':[]}}}
+
+Exemple:
+>>> starmap = {'GreatBear': {'stars': {}, 'adjacencies': {}}}
+>>> addStars(starmap, "GreatBear", ["Alkaid", "Dubhe"])
+>>> starmap
+{'GreatBear': {'stars': {'Alkaid': [], 'Dubhe': []}, 'adjacencies': {'Alkaid': [], 'Dubhe': []}}}
 """
 
 def addStars(starmap, constellation, starNames):
-    """
-    Afegeix les estrelles de la llista starNames a la constel·lació indicada del starmap.
-    Cada estrella s'inicialitza amb coordenades buides ([]) i sense adjacències ([]).
-    
-    """
-    if not starmap.get(constellation):
+    if constellation not in starmap:
         print("La constelacio no existeix")
-
     else:
         for star in starNames:
-            if starmap[constellation]['stars'].get(star):
+            if star in starmap[constellation]['stars']:
                 print("L'estrella " + star + " ja existeix a " + constellation)
             else:
                 starmap[constellation]['stars'][star] = []
