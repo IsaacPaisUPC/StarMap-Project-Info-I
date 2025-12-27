f81b2590bc74d533e0b56606f8124e52ea2c8bc3
txell0 <txell.sanchez@estudiantat.upc.edu>
2025-12-27 19:34:07 +0100

    Funcion_4.py

---
+++ funcions/Funcion_4.py
@@ -0,0 +1,18 @@
+"""
+Tasca 4
+Dissenyeu una funció de nom deleteAdjacency(starmap, constellation, star1, star2), que
+donat un mapa estel·lar, un nom de constel·lació, i dos noms d’estrella, elimini la connexió entre
+les dues estrelles donades
+Exemple:
+>>> starmap = {'Great Bear': {'stars': {'Alkaid': [], 'Dubhe': []},
+... 'adjacencies': {'Alkaid': ['Dubhe'], 'Dubhe': ['Alkaid']}}}
+>>> deleteAdjacency(starmap, "Great Bear", "Alkaid", "Dubhe")
+>>> starmap["Great Bear"]["adjacencies"]["Alkaid"]
+[]
+"""
+
+  def deleteAdjacency(starmap, constellation, star1, star2):
+    if constellation in starmap:
+        if star1 in starmap[constellation]['adjacencies'][star2] and star2 in starmap[constellation]['adjacencies'][star1]:
+            starmap[constellation]['adjacencies'][star1].remove(star2)
+            starmap[constellation]['adjacencies'][star2].remove(star1)
