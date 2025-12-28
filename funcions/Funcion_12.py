b5fb58a5c84598513a0026d8435efb80558a41ff
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2025-12-28 20:10:58 +0100

    Correct window setup in setupWindow function
    Fix setupWindow function to set window size correctly.

--- funcions/Funcion_12.py
+++ funcions/Funcion_12.py
@@ -5,8 +5,8 @@ la finestra de turtle, en fixi el títol i la mida, i retorni l’objecte Screen
 Nota: aquestes funcions visuals no tenen doctest.
 """
 import turtle
+
 def setupWindow(constellation, width=800, height=600):
-        screen = turtle.Screen()
-        screen.setup(width=800, height=600)
-        screen.title(constellation)
-        return screen
+    screen = turtle.Screen()
+    screen.title(constellation)
+    return screen
