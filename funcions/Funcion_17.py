8f72be125bd06ca028ae77d3b09eceb7f1b312c2
izanrodriguez03-hub <izanrodriguez03@gmail.com>
2025-12-29 15:58:01 +0100

    Create Funcion_17.py

---
+++ funcions/Funcion_17.py
@@ -0,0 +1,21 @@
+"""
+Tasca 17
+Dissenyeu una funció de nom drawAllSegments(pen, stars, adjacencies) que, donada una
+tortuga, un diccionari d’estrelles, i un diccionari d’adjacències, recorri totes les adjacències i dibuixi
+segments només quan should_draw_once(a,b) sigui True. Ignoreu noms sense coordenades vàlides.
+"""
+
+def drawAllSegments(pen, stars, adjacencies):
+    for estrella_1, conectades in adjacencies.items():
+
+        if estrella_1 not in stars or not isValidCoord(stars[estrella_1]):
+            continue
+
+        for estrella_2 in conectades:
+
+            if estrella_2 not in stars or not isValidCoord(stars[estrella_2]):
+                continue
+
+            if should_draw_once(estrella_1, estrella_2):
+                drawSegment( pen,stars[estrella_1],stars[estrella_2])
+
