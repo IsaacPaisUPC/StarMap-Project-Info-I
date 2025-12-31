320e8bcbdc3657601e0b886cfa24b1036ea6a0c6
txell0 <txell.sanchez@estudiantat.upc.edu>
2025-12-31 17:43:04 +0100

    Funcion_24.py

---
+++ funcions/Funcion_24.py
@@ -0,0 +1,20 @@
+"""
+Tasca 24
+Consultes — Comptar estrelles
+Implementeu countStars(starmap, constellation) que retorni el nombre total d’estrelles.
+Exemple:
+>>> starmap = {'Great Bear': {'stars': {'A':[], 'B':[], 'C':[]}, 'adjacencies':{}}}
+>>> countStars(starmap, "Great Bear")
+3
+"""
+def countStars(starmap, constellation):
+    """
+    >>> starmap = {'Great Bear': {'stars': {'A':[], 'B':[], 'C':[]}, 'adjacencies':{}}}
+    >>> countStars(starmap, "Great Bear")
+    3
+    """
+    contador = 0
+    for estrella in starmap[constellation]['stars']:
+        contador = contador + 1
+
+    return contador
