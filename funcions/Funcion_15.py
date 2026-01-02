de9024cb4f0ec07713a175abfc8f7430b30aaf15
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2026-01-02 13:46:44 +0100

    Update Funcion_15.py

--- funcions/Funcion_15.py
+++ funcions/Funcion_15.py
@@ -3,9 +3,9 @@ Tasca 15
 Dissenyeu una funció de nom drawAllStars(pen, stars) que, donada una tortuga i un diccionari
 d’estrelles (amb coordenades), dibuixi totes les estrelles amb coordenades vàlides [x, y].
 """
+
 def drawAllStars(pen, stars):
-        for estrella in stars:
-                coord= stars[estrella]
-                if isValidCoord(coord) == True:
-                        pen= makePen()
-                        drawStar(pen, estrella, coord, pointSize=8, offset=8)
+    for estrella in stars:
+        coord = stars[estrella]
+        if isValidCoord(coord):
+            drawStar(pen, estrella, coord)
