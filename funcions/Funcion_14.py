e4af228fba5e43a1893f39cb7bbfbefef39baf44
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2026-01-02 13:42:10 +0100

    Refactor drawStar function for coordinate handling

--- funcions/Funcion_14.py
+++ funcions/Funcion_14.py
@@ -1,13 +1,14 @@
 """
 Tasca 14
 Dissenyeu una funció de nom drawStar(pen, name, coord, pointSize=8, offset=8) que,
-donada una tortuga, un nom d’estrella, unes coordenades, la mida de punt i un desplaçament, dibuixi
+donada una tortuga, un nom d'estrella, unes coordenades, la mida de punt i un desplaçament, dibuixi
 un punt a coord i escrigui name desplaçat lleugerament per no tapar el punt.
 """
 
 def drawStar(pen, name, coord, pointSize=8, offset=8):
-    x,y = coord
-    pen.goto(x,y)
+    x = coord[0]
+    y = coord[1]
+    pen.goto(x, y)
     pen.dot(pointSize)
-    pen.goto(x+offset, y+offset)
+    pen.goto(x + offset, y + offset)
     pen.write(name)
