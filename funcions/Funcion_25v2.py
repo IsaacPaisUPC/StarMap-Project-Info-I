49a81e015897b81a5ed79bfd92acba9bdbb6ad49
Isaac Pais Alcalde <isaac.pais@estudiantat.upc.edu>
2026-01-02 16:07:35 +0100

    Refactor return statement for non-existent constellation

--- funcions/Funcion_25v2.py
+++ funcions/Funcion_25v2.py
@@ -7,14 +7,13 @@ def listIsolatedStars(starmap, constellation):
     llista = []
     if constellation not in starmap:
         print("La constel·lació no existeix")
-        return [] # Mejor devolver lista vacía que nada
+        return []
 
-    # Sacamos el diccionario de adjacencias para escribir menos
+
     adjs = starmap[constellation]['adjacencies']
 
     for estrella in starmap[constellation]['stars']:
-        # CASO 1: No está en el diccionario de adjacencias
-        # CASO 2: Está, pero su lista de vecinos está vacía (len == 0)
+      
         if estrella not in adjs or len(adjs[estrella]) == 0:
             llista.append(estrella)
             
